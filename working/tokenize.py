840d30e23e097e2755ba1f727aa7e28a01060fb3
SOONWOOK PARK <soonwook34@gmail.com>
2021-04-01 15:48:45 +0900

    이름 수정

--- working/tokenize.py
+++
@@ -1,4 +0,0 @@
-from konlpy.tag import Twitter as OriginalTwitter
-
-twitter_original = OriginalTwitter()
-print(twitter_original.pos('우리아이오아이는 정말 이뻐요'),'\n')
---
+++ working/tokenize_korean.py
@@ -0,0 +1,5 @@
+from konlpy.tag import Twitter
+
+twitter = Twitter()
+print(twitter.pos('우리아이오아이는 정말 이뻐요'))
+
